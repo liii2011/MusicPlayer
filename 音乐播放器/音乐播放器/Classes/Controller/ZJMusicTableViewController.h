d56bc17803fbd79dbc0ad1c3750dab2c0edb8d2a
liii <liii2011@163.com>
2015-11-29 17:06:08 +0800

    项目初始化 	1, 类前缀 	2, 应用图标 	3, 启动图片 	4, 文件分类 	5, 资源引入

---
+++ 音乐播放器/音乐播放器/Classes/Controller/ZJMusicTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  ZJMusicTableViewController.h
+//  音乐播放器
+//
+//  Created by liii on 15/11/29.
+//  Copyright © 2015年 liii. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZJMusicTableViewController : UITableViewController
+
+@end
---
+++ 音乐播放器/音乐播放器/Classes/Other/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  音乐播放器
+//
+//  Created by liii on 15/11/27.
+//  Copyright © 2015年 liii. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
